32b54e733cc7518d07e13836d02b45be61e22661
Mohammad Amin Nabian <m.a.nabiyan@gmail.com>
2025-10-27 11:11:43 -0700

    Update the end year in license headers (#1184)
    * update license headers
    
    * Revert "update license headers"
    
    This reverts commit 6c33bf517d27d81a9d780e51557c0ccbc8128710.
    
    * update license headers

--- examples/cfd/external_aerodynamics/domino/src/deprecated/train_sharded.py
+++ examples/cfd/external_aerodynamics/domino/src/deprecated/train_sharded.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: Copyright (c) 2023 - 2024 NVIDIA CORPORATION & AFFILIATES.
+# SPDX-FileCopyrightText: Copyright (c) 2023 - 2025 NVIDIA CORPORATION & AFFILIATES.
 # SPDX-FileCopyrightText: All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# SPDX-FileCopyrightText: Copyright (c) 2023 - 2024 NVIDIA CORPORATION & AFFILIATES.
+# SPDX-FileCopyrightText: Copyright (c) 2023 - 2025 NVIDIA CORPORATION & AFFILIATES.
 # SPDX-FileCopyrightText: All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
